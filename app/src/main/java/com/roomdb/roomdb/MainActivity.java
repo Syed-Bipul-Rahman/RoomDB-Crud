83ee9c8b72461598ad9a4b55787599dd84e6a74d
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-24 15:27:40 +0600

    Model class created

--- app/src/main/java/com/roomdb/roomdb/MainActivity.java
+++ app/src/main/java/com/roomdb/roomdb/MainActivity.java
@@ -3,12 +3,39 @@ package com.roomdb.roomdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    //declaring variables
+    EditText id,name,email,contact;
+    FloatingActionButton button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializations of  variables
+        id=findViewById(R.id.eid);
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        contact=findViewById(R.id.contact);
+        button=findViewById(R.id.addtocontactbtn);
+
+
+        //when user click on floating action button then contact should save in database
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/roomdb/roomdb/User.java
+++ app/src/main/java/com/roomdb/roomdb/User.java
@@ -1,4 +1,19 @@
 package com.roomdb.roomdb;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class User {
+    @PrimaryKey
+    public int uid;
+    @ColumnInfo(name = "name")
+    public String username;
+    @ColumnInfo(name = "eamil")
+    public String email;
+    @ColumnInfo(name = "contact")
+    public String contact;
+
+
 }
